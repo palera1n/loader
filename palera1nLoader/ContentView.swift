14efa0fec637750e38cad50be28b52ef43bb45ef
Lakhan Lothiyi <42747613+llsc12@users.noreply.github.com>
2022-11-11 19:29:04 +0000

    Initial Commit

---
+++ palera1nLoader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  palera1nLoader
+//
+//  Created by Lakhan Lothiyi on 11/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ palera1nLoader/palera1nLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  palera1nLoaderApp.swift
+//  palera1nLoader
+//
+//  Created by Lakhan Lothiyi on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct palera1nLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
