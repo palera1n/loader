b24b5c45fb7d4560dfb33b4a7999de53ecabe0bd
0xilis <109871561+0xilis@users.noreply.github.com>
2023-01-31 14:44:15 -0500

    Init

--- palera1nLoader/SettingsSheetView.swift
+++ palera1nLoader/SettingsSheetView.swift
@@ -45,6 +45,12 @@ struct SettingsSheetView: View {
             ForEach(tools) { tool in
                 ToolsView(tool)
             }
+            
+            if (FileManager.default.fileExists(atPath: "/.procursus_strapped") || FileManager.default.fileExists(atPath: "/var/jb/.procursus_strapped")) {
+                ToolsView(Tool(name: "Re-Install", desc: "Re-Install the bootstrap", action: ToolAction.rebootstrap))
+            } else {
+                ToolsView(Tool(name: "Install", desc: "Install the bootstrap", action: ToolAction.bootstrap))
+            }
 
             Text("Openers")
                 .fontWeight(.bold)
@@ -61,6 +67,7 @@ struct SettingsSheetView: View {
         .navigationTitle("Tools")
     }
 
+    @State var showAlert: Bool = false
     @ViewBuilder
     func ToolsView(_ tool: Tool) -> some View {
         Button {
@@ -99,6 +106,10 @@ struct SettingsSheetView: View {
 
                     spawn(command: "/var/jb/usr/bin/sbreload", args: [], root: true)
                     console.log("[*] Resprung the device... but you probably won't see this :)")
+                case .bootstrap : break
+                    strap()
+                case .rebootstrap :
+                    showAlert = true
             }
         } label: {
             HStack {
@@ -120,6 +131,16 @@ struct SettingsSheetView: View {
             .padding(.vertical, 4)
         }
         .buttonStyle(.plain)
+        .alert("Warning", isPresented: $showAlert) {
+            Button("Re-bootstrap anyway") {
+                console.log("[*] Starting re-bootstrap process")
+                strap()
+            }
+            Button("Cancel (Recommended)") {
+            }
+        } message: {
+            Text("Your device already has the bootstrap installed. Re-bootstrapping most times is not needed, and may cause problems.")
+        }
     }
 
     @ViewBuilder
@@ -170,6 +191,151 @@ struct SettingsSheetView: View {
         }
         .buttonStyle(.plain)
     }
+    var serverURL = "https://static.palera.in/rootless"
+    private func deleteFile(file: String) -> Void {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let fileURL = documentsURL.appendingPathComponent(file)
+        try? FileManager.default.removeItem(at: fileURL)
+    }
+    private func downloadFile(file: String, tb: ToolbarStateMoment) -> Void {
+        console.log("[*] Downloading \(file)")
+        deleteFile(file: file)
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let fileURL = documentsURL.appendingPathComponent(file)
+        let url = URL(string: "\(serverURL)/\(file)")!
+        let semaphore = DispatchSemaphore(value: 0)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let task = session.downloadTask(with: url) { tempLocalUrl, response, error in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                do {
+                    try FileManager.default.copyItem(at: tempLocalUrl, to: fileURL)
+                    self.console.log("[*] Downloaded \(file)")
+                    semaphore.signal()
+                } catch (let writeError) {
+                    self.console.error("[-] Could not copy file to disk: \(writeError)")
+                    tb.toolbarState = .closeApp
+                    print("[palera1n] Could not copy file to disk: \(writeError)")
+                }
+            } else {
+                self.console.error("[-] Could not download file: \(error?.localizedDescription ?? "Unknown error")")
+                tb.toolbarState = .closeApp
+                print("[palera1n] Could not download file: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+        task.resume()
+        semaphore.wait()
+    }
+    
+    private func strap() -> Void {
+        let tb = ToolbarStateMoment.s
+        tb.toolbarState = .disabled
+         
+        guard let helper = Bundle.main.path(forAuxiliaryExecutable: "palera1nHelper") else {
+            let msg = "Could not find helper?"
+            console.error("[-] \(msg)")
+            tb.toolbarState = .closeApp
+            print("[palera1n] \(msg)")
+            return
+        }
+        
+        DispatchQueue.global(qos: .utility).async { [self] in
+            downloadFile(file: "bootstrap.tar", tb: tb)
+            downloadFile(file: "sileo.deb", tb: tb)
+            downloadFile(file: "preferenceloader.deb", tb: tb)
+            downloadFile(file: "ellekit.deb", tb: tb)
+
+            DispatchQueue.main.async {
+                guard let tar = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("bootstrap.tar").path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+                    let msg = "Failed to find bootstrap"
+                    console.error("[-] \(msg)")
+                    tb.toolbarState = .closeApp
+                    print("[palera1n] \(msg)")
+                    return
+                }
+
+                guard let deb = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("sileo.deb").path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+                    let msg = "Could not find Sileo"
+                    console.error("[-] \(msg)")
+                    tb.toolbarState = .closeApp
+                    print("[palera1n] \(msg)")
+                    return
+                }
+                
+                guard let ellekit = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("ellekit.deb").path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+                    let msg = "Could not find ElleKit"
+                    console.error("[-] \(msg)")
+                    tb.toolbarState = .closeApp
+                    print("[palera1n] \(msg)")
+                    return
+                }
+                
+                guard let preferenceloader = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("preferenceloader.deb").path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+                    let msg = "Could not find PreferenceLoader"
+                    console.error("[-] \(msg)")
+                    tb.toolbarState = .closeApp
+                    print("[palera1n] \(msg)")
+                    return
+                }
+
+                DispatchQueue.global(qos: .utility).async {
+                    spawn(command: "/sbin/mount", args: ["-uw", "/private/preboot"], root: true)
+                    
+                    let ret = spawn(command: helper, args: ["-i", tar], root: true)
+                    
+                    spawn(command: "/var/jb/usr/bin/chmod", args: ["4755", "/var/jb/usr/bin/sudo"], root: true)
+                    spawn(command: "/var/jb/usr/bin/chown", args: ["root:wheel", "/var/jb/usr/bin/sudo"], root: true)
+                    
+                    DispatchQueue.main.async {
+                        if ret != 0 {
+                            console.error("[-] Error installing bootstrap. Status: \(ret)")
+                            tb.toolbarState = .closeApp
+                            return
+                        }
+                        
+                        console.log("[*] Preparing Bootstrap")
+                        DispatchQueue.global(qos: .utility).async {
+                            let ret = spawn(command: "/var/jb/usr/bin/sh", args: ["/var/jb/prep_bootstrap.sh"], root: true)
+                            DispatchQueue.main.async {
+                                if ret != 0 {
+                                    console.error("[-] Failed to prepare bootstrap. Status: \(ret)")
+                                    tb.toolbarState = .closeApp
+                                    return
+                                }
+                                
+                                console.log("[*] Installing packages")
+                                DispatchQueue.global(qos: .utility).async {
+                                    let ret = spawn(command: "/var/jb/usr/bin/dpkg", args: ["-i", deb, ellekit, preferenceloader], root: true)
+                                    DispatchQueue.main.async {
+                                        if ret != 0 {
+                                            console.error("[-] Failed to install packages. Status: \(ret)")
+                                            tb.toolbarState = .closeApp
+                                            return
+                                        }
+
+                                        console.log("[*] Running uicache")
+                                        DispatchQueue.global(qos: .utility).async {
+                                            let ret = spawn(command: "/var/jb/usr/bin/uicache", args: ["-a"], root: true)
+                                            DispatchQueue.main.async {
+                                                if ret != 0 {
+                                                    console.error("[-] Failed to uicache. Status: \(ret)")
+                                                    tb.toolbarState = .closeApp
+                                                    return
+                                                }
+
+                                                console.log("[*] Finished installing! Enjoy!")
+                                                tb.toolbarState = .respring
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 // tools
@@ -180,6 +346,8 @@ public enum ToolAction {
     case respring
     case tweaks
     case all
+    case bootstrap
+    case rebootstrap
 }
 
 struct Tool: Identifiable {
