782a9b8d91c1efc2bc96b276e497c50b12517c95
Tom E <guacaplushy@icloud.com>
2023-02-05 00:59:50 -0500

    force revert

--- palera1nHelper/paleinfo/get.c
+++ palera1nHelper/paleinfo/get.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <stdint.h>
 
+#define checkrain_option_safemode            (1 << 0)
+#define checkrain_option_bind_mount          (1 << 1)
+#define checkrain_option_overlay             (1 << 2)
+#define checkrain_option_force_revert        (1 << 7) /* keep this at 7 */
+    
 #define palerain_option_rootful              (1 << 0) /* rootful jailbreak */
 #define palerain_option_jbinit_log_to_file   (1 << 1) /* log to /cores/jbinit.log */
 #define palerain_option_setup_rootful        (1 << 2) /* create fakefs */
@@ -18,9 +23,50 @@ struct paleinfo {
     char rootdev[0x10];
 };
 
+struct kerninfo {
+    uint64_t size;
+    uint64_t base;
+    uint64_t slide;
+    checkrain_option_t flags;
+};
+
 static inline bool checkrain_option_enabled(checkrain_option_t flags, checkrain_option_t opt)
 {
     return (flags & opt) != 0;
+}
+
+extern int get_fr() {
+    FILE *rd = fopen("/dev/rmd0", "rb");
+    if (rd == NULL) {
+        return 1;
+    }
+
+    fseek(rd, 0, SEEK_END);
+    long size = ftell(rd);
+    fseek(rd, 0, SEEK_SET);
+    
+    if (size < 0x1004) {
+        return 1;
+    }
+
+    char *sizeBytes = malloc(sizeof(char) * 4);
+    assert(sizeBytes != NULL);
+    fread(sizeBytes, 1, 4, rd);
+
+    uint32_t rdRealSize = *(uint32_t *)sizeBytes;
+
+    printf("size is %d\n", rdRealSize);
+    fseek(rd, rdRealSize, SEEK_SET);
+
+    char *dataRead = malloc(sizeof(struct kerninfo));
+    assert(dataRead != NULL);
+    fread(dataRead, 1, sizeof(struct kerninfo), rd);
+
+    struct kerninfo *readStruct = (struct kerninfo *)dataRead;
+
+    fclose(rd);
+  
+    return checkrain_option_enabled(readStruct->flags, checkrain_option_force_revert) ? 1 : 0;
 }
 
 extern int get_rootful() {
