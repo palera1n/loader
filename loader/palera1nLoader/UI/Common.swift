338aa97431b40167cab55c9c4e8fef3e00c48017
flower <97859147+flowerible@users.noreply.github.com>
2023-04-23 10:35:13 -0700

    joestupid

--- loader/palera1nLoader/UI/Common.swift
+++ loader/palera1nLoader/UI/Common.swift
@@ -84,16 +84,20 @@ func errAlert(title: String, message: String) {
 }
 // image mods
 func applySymbolModifications(to cell: UITableViewCell, with symbolName: String, backgroundColor: UIColor) {
-    let symbolConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .medium)
+    let symbolConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .medium)
     let symbolImage = UIImage(systemName: symbolName, withConfiguration: symbolConfig)?
         .withTintColor(.white, renderingMode: .alwaysOriginal)
-    let coloredBackgroundImage = UIGraphicsImageRenderer(size: CGSize(width: 30, height: 30)).image { context in
+    let symbolSize = symbolImage?.size ?? .zero
+    let imageSize = CGSize(width: 30, height: 30)
+    let scale = min((imageSize.width - 6) / symbolSize.width, (imageSize.height - 6) / symbolSize.height)
+    let adjustedSymbolSize = CGSize(width: symbolSize.width * scale, height: symbolSize.height * scale)
+    let coloredBackgroundImage = UIGraphicsImageRenderer(size: imageSize).image { context in
         backgroundColor.setFill()
-        UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 30, height: 30), cornerRadius: 7).fill()
+        UIBezierPath(roundedRect: CGRect(origin: .zero, size: imageSize), cornerRadius: 7).fill()
     }
-    let mergedImage = UIGraphicsImageRenderer(size: CGSize(width: 30, height: 30)).image { context in
-        coloredBackgroundImage.draw(in: CGRect(x: 0, y: 0, width: 30, height: 30))
-        symbolImage?.draw(in: CGRect(x: 5, y: 5, width: 20, height: 20)) // adjust the x and y values as needed
+    let mergedImage = UIGraphicsImageRenderer(size: imageSize).image { context in
+        coloredBackgroundImage.draw(in: CGRect(origin: .zero, size: imageSize))
+        symbolImage?.draw(in: CGRect(x: (imageSize.width - adjustedSymbolSize.width) / 2, y: (imageSize.height - adjustedSymbolSize.height) / 2, width: adjustedSymbolSize.width, height: adjustedSymbolSize.height))
     }
     cell.imageView?.image = mergedImage
     cell.imageView?.layer.cornerRadius = 7
